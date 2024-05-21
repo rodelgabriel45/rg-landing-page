a4795dd7b27e4a1803743b2163773e9352817aea
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-21 19:48:25 +0800

    create server and auth routes

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,70 @@
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const register = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (user) return next(errorHandler(400, "User Already exist!"));
+
+    if (!username || !password)
+      return next(errorHandler(400, "All fields are required."));
+    if (username.length < 6)
+      return next(errorHandler(400, "Username must be atleast 6 characters."));
+    if (password.length < 7)
+      return next(errorHandler(400, "Password must be atleast 7 characters."));
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    const { password: pass, ...rest } = newUser._doc;
+
+    res.status(201).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, user.password);
+
+    if (!validPassword)
+      return next(errorHandler(400, "Invalid user credentials."));
+
+    const { password: pass, ...rest } = user._doc;
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out.");
+  } catch (error) {
+    next(error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,11 +2,28 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// API routes
+app.use("/api/auth", authRoutes);
+
+// Middleware for handing errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 mongoose
   .connect(process.env.MONGO)
